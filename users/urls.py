caefc36428e8b6f8763824a3850288955db90a54
Bohdan Romanov <bytovskyden@gmail.com>
2026-08-14 13:25:42 +0300

    feat(users): implement users app with authentication, registration and tests

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,9 @@ from users.views import CreateUserView, UserMeView
 urlpatterns = [
     path("", CreateUserView.as_view(), name="register"),
     path("me/", UserMeView.as_view(), name="me"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", TokenObtainPairView.as_view(), name="token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
 ]
+
+app_name = "users"
