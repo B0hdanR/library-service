eb154783d5886c61e28b6b8a64cf207610300446
Bohdan Romanov <bytovskyden@gmail.com>
2026-08-15 11:11:16 +0300

    add book model and migrations, add books in INSTALLED_APPS

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverType.choices,  # noqa
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.00"))],
+    )
+
+    def __str__(self):
+        return self.title
