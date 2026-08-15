8948966dd6b54c0a0e745f2fe98451db34c52014
Bohdan Romanov <bytovskyden@gmail.com>
2026-08-15 15:10:36 +0300

    feat(borrowings): implemented the list and detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+@extend_schema_view(
+    list=extend_schema(
+        tags=["Borrowings"],
+        description="Retrieve a list of borrowings.",
+    ),
+    retrieve=extend_schema(
+        tags=["Borrowings"],
+        description="Retrieve detailed information about a specific borrowing.",
+    ),
+)
+class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingListSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingListSerializer
