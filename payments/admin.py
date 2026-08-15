76a3590807287a86a24f1845fd85c217697fd9aa
Bohdan Romanov <bytovskyden@gmail.com>
2026-08-15 19:32:59 +0300

    feat(payments): register payment in admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "type",
+        "borrowing",
+        "money_to_pay",
+    )
+
+    list_filter = (
+        "status",
+        "type",
+    )
