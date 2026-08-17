711ef96b602bca0438d888750b1b5a982b259559
Bohdan Romanov <bytovskyden@gmail.com>
2026-08-17 14:46:51 +0300

    feat(borrowings): add get_queryset to filter that the user can see only his own borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,3 @@
-from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,28 +9,19 @@ from borrowings.serializers import (
 )
 
 
-@extend_schema_view(
-    list=extend_schema(
-        tags=["Borrowings"],
-        description="Retrieve a list of borrowings.",
-    ),
-    retrieve=extend_schema(
-        tags=["Borrowings"],
-        description="Retrieve detailed information about a specific borrowing.",
-    ),
-    create=extend_schema(
-        tags=["Borrowings"],
-        description=(
-            "Create a new borrowing for the authenticated user. "
-            "The book inventory is decreased by one."
-        ),
-    ),
-)
 class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingListSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ["get", "post", "head", "options"]
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.select_related("book", "user").prefetch_related(
+            "payments"
+        )
+        user = self.request.user
+        if user.is_staff:
+            return queryset
+        return queryset.filter(user=user)
 
     def get_serializer_class(self):
         if self.action == "create":
